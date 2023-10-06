1010877ce906e1c1e8283829ba4c85d2d9ae9a45
McCookieM <McCookieM@users.noreply.github.com>
2023-10-06 09:15:08 +0100

    Update const.py

--- custom_components/uk_fuel_prices/const.py
+++ custom_components/uk_fuel_prices/const.py
@@ -1,6 +1,16 @@
 """Define constants used in uk_fuel_prices."""
-from homeassistant.helpers import selector
 
-# Constants for uk_fuel_prices.
-# Base component constants
+import logging
+
 DOMAIN = "uk_fuel_prices"
+
+CONF_RETAILERS = "retailers"
+CONF_FUEL_TYPE = "fuel_type"
+CONF_UPDATE_FREQUENCY = "update_frequency"
+
+SENSOR_PREFIX = "UK Fuel Price "
+
+GOV_LIST = "https://www.gov.uk/guidance/access-fuel-price-data"
+TABLE_XPATH = "/html/body/div[3]/main/div[4]/div[1]/div/div[2]/div/table"
+
+_LOGGER = logging.getLogger(__name__)
